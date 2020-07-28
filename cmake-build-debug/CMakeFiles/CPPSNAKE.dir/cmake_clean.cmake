b3b70c8f1409f4a00b300a31e897e648ebc50cf9
Walter Nam <wkn2@case.edu>
2020-07-28 21:48:24 +0900

    Add files via upload

---
+++ cmake-build-debug/CMakeFiles/CPPSNAKE.dir/cmake_clean.cmake
@@ -0,0 +1,12 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/CPPSNAKE.dir/main.cpp.obj"
+  "CPPSNAKE.exe"
+  "CPPSNAKE.exe.manifest"
+  "CPPSNAKE.pdb"
+  "libCPPSNAKE.dll.a"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/CPPSNAKE.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
---
+++ main.cpp
@@ -0,0 +1,27 @@
+#include <iostream>
+using namespace std;
+bool gameOver;
+void Setup(){
+    gameOver = false;
+}
+
+void Draw(){
+
+}
+
+void Input(){
+
+}
+
+void Logic(){
+
+}
+
+int main() {
+    Setup();
+    while(! gameOver){
+        Draw();
+        Input();
+    }
+    return 0;
+}
