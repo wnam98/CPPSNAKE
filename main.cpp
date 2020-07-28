a8bf087bf70bc0c5b3097d467a8847cadcb102d9
Walter Nam <49884140+wnam98@users.noreply.github.com>
2020-07-28 22:21:49 +0900

    built the grid layout and added the draw methods

--- main.cpp
+++ main.cpp
@@ -1,12 +1,47 @@
 #include <iostream>
 using namespace std;
+
 bool gameOver;
+const int width = 20;
+const int height = 20;
+int x, y, fruitX, fruitY, score;
+enum eDirection {STOP = 0, LEFT, RIGHT, UP, DOWN};
+eDirection dir;
+
 void Setup(){
     gameOver = false;
+    dir = STOP;
+    x = width/2;
+    y = height/2;
+    fruitX = rand() % width;
+    fruitY = rand() % height;
+    score = 0;
 }
 
 void Draw(){
+    system("cls");
+    for(int i = 0; i < width + 2; i++){
+        cout << "#";
+    }
+    cout << endl;
 
+    for(int i = 0; i < height; i++){
+        for(int j = 0; j < width; j++){
+            if(j == 0)
+                cout << "#";
+            else
+                cout << " ";
+            if(j == width - 1)
+                cout << "#";
+        }
+        cout << endl;
+    }
+
+
+    for(int i = 0; i < width + 2; i++){
+        cout << "#";
+    }
+    cout << endl;
 }
 
 void Input(){
@@ -22,6 +57,8 @@ int main() {
     while(! gameOver){
         Draw();
         Input();
+        Logic();
+        //Sleep(10);
     }
     return 0;
 }
